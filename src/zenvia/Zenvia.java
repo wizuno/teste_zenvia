a2912baa176e61dabf81160bc09c46aebdab9720
willhs <willhs@willhs>
2017-06-05 14:24:17 -0300

    Inicial do projeto

---
+++ src/zenvia/Zenvia.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zenvia;
+
+/**
+ *
+ * @author willhs
+ */
+public class Zenvia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int i=1; i <101; i++){
+            if(i%3 == 0){
+                System.out.println("Fizz");
+            }
+            else{
+                if(i%5 == 0){
+                    System.out.println("Buzz");
+                }
+                else{
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+    
+}
