eab35677b0d1e584b609b8c181d8793bb933c075
willhs <willhs@willhs>
2017-06-05 15:03:56 -0300

    Ultima versão

--- src/zenvia/Zenvia.java
+++ src/zenvia/Zenvia.java
@@ -18,16 +18,16 @@ public class Zenvia {
         // TODO code application logic here
         
         for(int i=1; i <101; i++){
-            if(i%3 == 0){
-                System.out.println("Fizz");
+            if(i%3 == 0 && i%5 == 0){
+                System.out.println("FizzBuzz");
             }
             else{
-                if(i%5 == 0){
-                    System.out.println("Buzz");
+                if(i%3 == 0){
+                    System.out.println("Fizz");
                 }
                 else{
-                    if(i%3 == 0 && i%5 == 0 ){
-                        System.out.println("FizzBuzz");
+                    if(i%5 == 0){
+                        System.out.println("Buzz");
                     }
                     else{
                         System.out.println(i);
